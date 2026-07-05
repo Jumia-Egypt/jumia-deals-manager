777ce28364f66809c82b72c327eefdc98fef6781
Jumia-Egypt <george.ayman@jumia.com>
2026-07-05 22:11:10 +0300

    deploy: add serverless API - api/_supabase.js

---
+++ api/_supabase.js
@@ -0,0 +1,9 @@
+// Shared Supabase client for all API functions
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  'https://rredzebycviyqakevzdl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZWR6ZWJ5Y3ZpeXFha2V2emRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjk2MzAsImV4cCI6MjA5ODg0NTYzMH0.b7quMoIeurfy_B5Rl3o85RrbFbLUBGsaNZLhJMdiREQ'
+);
+
+module.exports = { supabase };
