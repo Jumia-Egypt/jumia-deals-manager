4e6c66aa68c026ba58bc6605f5b08166bbc0722c
Jumia-Egypt <george.ayman@jumia.com>
2026-07-05 23:02:43 +0300

    fix: CJS conversion (api/_supabase.js)

--- api/_supabase.js
+++ api/_supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
   'https://rredzebycviyqakevzdl.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZWR6ZWJ5Y3ZpeXFha2V2emRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjk2MzAsImV4cCI6MjA5ODg0NTYzMH0.b7quMoIeurfy_B5Rl3o85RrbFbLUBGsaNZLhJMdiREQ'
 );
 
-export { supabase };
+module.exports = { supabase };
